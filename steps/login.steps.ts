a35fb897f47fd08f5e9380feb6825c15d945d509
tejas1coder <tejasjbs1948@gmail.com>
2025-11-06 12:56:27 -0500

    Update login.steps.ts

--- steps/login.steps.ts
+++ steps/login.steps.ts
@@ -1,11 +1,15 @@
-import { Then } from '@cucumber/cucumber';
+import { Then, When } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Login } from '../pages/login.page';
 
-Then('I should see the title {string}', async (expectedTitle) => {
+Then('I should see the title {string}', async (expectedTitle: string) => {
   await new Login(getPage()).validateTitle(expectedTitle);
 });
 
-Then('I will login as {string}', async (userName) => {
+When('I will login as {string}', async (userName: string) => {
+  await new Login(getPage()).loginAsUser(userName);
+});
+
+Then('I should get an error message as {string}', async (userName: string) => {
   await new Login(getPage()).loginAsUser(userName);
 });
