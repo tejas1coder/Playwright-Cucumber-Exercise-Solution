1ace9d18641f583194dc6a4f3142696161409a2a
tejas1coder <tejasjbs1948@gmail.com>
2025-11-06 12:49:57 -0500

    Update login.page.ts

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,4 +1,5 @@
 import { Page } from "@playwright/test"
+import { expect } from '@cucumber/cucumber';
 
 export class Login {
     private readonly page: Page
@@ -6,6 +7,8 @@ export class Login {
     private readonly passwordField: string = 'input[id="password"]'
     private readonly userNameField: string = 'input[id="user-name"]'
     private readonly loginButton: string = 'input[id="login-button"]'
+    private readonly errorMessageField: string = '.error-message-container'
+
 
     constructor(page: Page) {
         this.page = page;
@@ -22,5 +25,10 @@ export class Login {
         await this.page.locator(this.userNameField).fill(userName)
         await this.page.locator(this.passwordField).fill(this.password)
         await this.page.locator(this.loginButton).click()
+    }
+
+    public async validateLoginErrorMessage(expectedMessage: string) {
+        const actualMessage = await this.page.locator(this.errorMessageField).textContent();
+        expect(actualMessage?.trim()).toBe(expectedMessage);
     }
 }
